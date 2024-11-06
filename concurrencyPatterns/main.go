631bc240cc9a8ef8aee5ed77a96ca0fb9f755d02
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2024-11-06 13:29:28 +0545

    pipeline model

--- concurrencyPatterns/main.go
+++ concurrencyPatterns/main.go
@@ -1,12 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 
-	fmt.Println("Hii")
-	fmt.Println("This is from channel")
-	channel()
+	// fmt.Println("Hii")
+	// fmt.Println("This is from channel")
+	// channel()
+	// forslelctLoop1()
+
+	// Input
+	numbers := []int{2, 3, 4, 5, 6}
+
+	//stage 1
+	dataChannel := slicetoChannel(numbers)
+
+	//stage 2
+	finalChannel := sq(dataChannel)
+
+	//stage 3
+	for n := range finalChannel {
+		fmt.Println(n)
+	}
 }
