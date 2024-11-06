3f0823992ec56ee9d35ccb6adbd1b1b461818793
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2024-11-06 12:32:57 +0545

    forselectloop concurrency pattern

--- concurrencyPatterns/forselectLoop.go
+++ concurrencyPatterns/forselectLoop.go
@@ -1,7 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func forslelctLoop() {
 
 	//Buffered channels because of 3 in the last
 	charChannel := make(chan string, 3)
+	chars := []string{"hello", "how", "are"}
+
+	for _, s := range chars {
+		select {
+		case charChannel <- s:
+		}
+	}
+	close(charChannel)
+
+	for result := range charChannel {
+		fmt.Println(result)
+	}
+}
+
+func forslelctLoop1() {
+	go func() {
+		for {
+			select {
+			default:
+				fmt.Println("Doing work")
+			}
+		}
+	}()
+	time.Sleep(time.Second * 4)
 }
