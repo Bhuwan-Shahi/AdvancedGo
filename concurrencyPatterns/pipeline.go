fbf5e01b31abff2be262f63fbdbeaaff7472d29c
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2024-11-06 13:27:03 +0545

    pipeline model

---
+++ concurrencyPatterns/pipeline.go
@@ -0,0 +1,24 @@
+package main
+
+func slicetoChannel(num []int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range num {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+
+	}()
+	return out
+}
